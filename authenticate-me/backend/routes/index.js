aa2543b3e0d209c4415d917c0f94f4da3822b62f
jamie <jzrust@gmail.com>
2021-02-20 11:32:56 -0500

    didnt successfully test csrf, starting phase 1

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -6,13 +6,39 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../utils/auth.js');
 const { User } = require('../db/models');
 
-
-
-
-
-
-
-
 router.use('/api', apiRouter)
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.json({});
+    });
+  }
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path')
+    //serve the frontends index.html at the root route
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken())
+        return res.sendFile(
+            path.resolve(_dirname, '../../frontend', 'build', 'index.html')
+        )
+    })
+
+    //serves the static assets in the frontends build folder
+    router.use(express.static(path.resolve("../frontend/build")))
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+
+
 
 module.exports = router
